72f9bf429f8783d62f5be44ca828410e6610cb3a
showmiya13 <showmishowmiya23@gmail.com>
2023-03-02 12:58:16 +0530

    First

---
+++ src/org/scan/Employee.java
@@ -0,0 +1,16 @@
+package org.scan;
+
+import java.util.Scanner;
+
+public class Employee {
+private static Scanner s;
+
+public static void main(String[] args) {
+	s = new Scanner(System.in);
+	System.out.println("Enter the employee id");
+	byte a = s.nextByte();
+	System.out.println("empid="+a );
+	System.out.println("enter the employee name");
+	String n = s.next();
+}
+}
